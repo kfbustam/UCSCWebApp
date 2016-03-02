41fe7cb863cc41325323feaeb6cae1b7b78a2518
Josef Bustamante <kfbustam@ucsc.edu>
2016-03-02 15:07:39 -0800

    fixed merge conflicts

--- js/dining/dining_main.js
+++ js/dining/dining_main.js
@@ -1,11 +1,6 @@
 function main() {
-  var diningListModel = newDiningListModel(newDiningListApi());
-<<<<<<< HEAD
-  var diningListView = newDiningListView();
-  newDiningListController(diningListModel, diningListView);
-=======
   var diningHallModel = newDiningHallModel(newDiningHallApi());
   var diningListView = newDiningListView();
   newDiningListController(diningListModel, diningHallModel, diningListView);
->>>>>>> origin/Develop
+
 }
