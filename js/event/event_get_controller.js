dc5ab8bc131fd4f8edb6bc379b6faaee461c80cb
isayyuhh-s <isaiahsolomon16@gmail.com>
2016-03-02 16:24:48 -0800

    event stuffs

--- js/event/event_get_controller.js
+++ js/event/event_get_controller.js
@@ -14,13 +14,16 @@ function newEventGetController(model, view) {
         var li_ul = document.createElement("ul");
         var li_ul_li = document.createElement("li");
         var br = document.createElement("br");
+        var hr = document.createElement("hr");
         li.setAttribute("id", "event-name");
         li.appendChild(document.createTextNode(events[i].getName() + "\n"));
         li_ul_li.setAttribute("id", "event-detail");
         li_ul_li.appendChild(document.createTextNode(events[i].getDescription()));
+        
         li_ul.appendChild(li_ul_li);
         li.appendChild(li_ul);
         li.appendChild(br);
+        li.appendChild(hr);
         ul.appendChild(li);
       }
     });
