99918e094a2dd39f6138b36e03b29e2320415c6e
Megan Nguyen <mdptnguyen@gmail.com>
2016-03-05 14:40:17 -0800

    updated/created css files, finished dining display

--- js/dining/dining_list_controller.js
+++ js/dining/dining_list_controller.js
@@ -33,7 +33,52 @@ function newDiningListController(dlmodel, dhmodel, view) {
         li.appendChild(document.createTextNode(foodName));
         for (k=0;k<foodAttr.length;k++) {
           li.appendChild(br);
-          li.appendChild(document.createTextNode(foodAttr[k]));
+		  
+		  
+		  /* changes attribute strings to the appropriate images*/		  
+		  var aimg = document.createElement('img');
+
+		  if(foodAttr[k] == 'eggs'){
+			  aimg.src = 'css/attributes/eggs.png';
+			  aimg.alt = 'eggs';
+		  }
+		  if(foodAttr[k] == 'fish'){
+			  aimg.src = 'css/attributes/fish.png';
+			  aimg.alt = 'fish';
+		  }
+		  if(foodAttr[k] == 'gluten'){
+			  aimg.src = 'css/attributes/glutenf.png';
+			  aimg.alt = 'gluten free';
+		  }
+		  if(foodAttr[k] == 'milk'){
+			  aimg.src = 'css/attributes/dairy.png';
+			  aimg.alt = 'dairy';
+		  }
+		  if(foodAttr[k] == 'nuts'){
+			  aimg.src = 'css/attributes/nuts.png';
+			  aimg.alt = 'nuts';
+		  }
+		  if(foodAttr[k] == 'soy'){
+			  aimg.src = 'css/attributes/soy.png';
+			  aimg.alt = 'soy';
+		  }
+		  if(foodAttr[k] == 'veggie'){
+			  aimg.src = 'css/attributes/vegetarian.png';
+			  aimg.alt = 'vegetarian';
+		  }
+		  if(foodAttr[k] == 'vegan'){
+			  aimg.src = 'css/attributes/vegan.png';
+			  aimg.alt = 'vegan';
+		  }
+		  if(foodAttr[k] == 'pork'){
+			  aimg.src = 'css/attributes/pork.png';
+			  aimg.alt = 'pork';
+		  }
+		  if(foodAttr[k] == 'beef'){
+			  aimg.src = 'css/attributes/beef.png';
+			  aimg.alt = 'beef';
+		  }
+          li.appendChild(aimg);
         }
         ul.appendChild(li);
       }
